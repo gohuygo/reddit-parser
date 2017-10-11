58eef4f0f896931d6855f8d20151c648025f8196
Huy Nguyen <huy@gohuygo.com>
2017-10-10 22:32:11 -0700

    snoowrap and dotenv

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 const program = require('commander');
 const pkg = require('./package.json');
 const snoowrap = require('snoowrap');
-
+const reddit = new snoowrap({
+  userAgent: 'put your user-agent string here',
+  clientId: 'put your client id here',
+  clientSecret: 'put your client secret here',
+  refreshToken: 'put your refresh token here'
+});
 program
   .version(pkg.version)
   .description(pkg.description)
@@ -11,8 +16,10 @@ program
 
 program
   .command('scrape [thread]')
-  .description('scrape all 1st-level posts from thread')
-  .action((thread ='/') => console.log('thread', thread))
+  .description('scrape all comments from thread')
+  .action((thread ='/') => {
+
+  })
 
 program.parse(process.argv);
 if (!program.args.filter(arg => typeof arg === 'object').length) {
