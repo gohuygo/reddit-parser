25fc4b864aaaa0dfa1ae1020ee3ca0cb447ca54f
Huy Nguyen <huy@gohuygo.com>
2017-10-19 20:58:19 -0700

    get list of thread ids

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const program = require('commander');
 const pkg = require('./package.json');
 const moment = require('moment');
 const snoowrap = require('snoowrap');
-const reddit = new snoowrap({
+
+const reddita = new snoowrap({
   userAgent: process.env.REDDITUSERAGENT,
   clientId: process.env.REDDITCLIENTID,
   clientSecret: process.env.REDDITCLIENTSECRET,
@@ -23,18 +24,20 @@ program
   .command('scrape [thread]')
   .description('scrape all comments from thread')
   .action((thread ='/') => {
-    const ethtrader = reddit.getSubreddit('ethtrader')
+    const ethtrader = reddita.getSubreddit('ethtrader')
 
-    let arr = new Array(50);
+    let numberOfDaysBack = 50;
+    let arrIds           = new Array(0);
 
-    for (var i = 0; i < arr.length; i++) {
-      let date = moment().subtract(0-i, 'days').format('MMMM D, YYYY')
-      let dailyThreadId = ethtrader.search({query: 'Daily General Discussion - ' + date})
-                                   .then(a=>Promise.resolve(a).then(b=>console.log(b[0]['id'])))
-      arr[i] = dailyThreadId
+    for (var i = 0; i < numberOfDaysBack; i++) {
+      let date        = moment().subtract(0+i, 'days').format('MMMM D, YYYY')
+      let dailyThread = ethtrader.search({query: 'Daily General Discussion - ' + date})
+
+      Promise.resolve(dailyThread).then(a=>{
+        arrIds.push(a[0].id)
+      })
     }
 
-    console.log(arr)
     // reddit.getSubmission('4j8p6d').expandReplies({limit: Infinity, depth: Infinity}).then(console.log)
 
     // console.log(ethtrader)
